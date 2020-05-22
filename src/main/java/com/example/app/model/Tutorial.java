c422dc8dea1513ee510a6ca4d922f2930f09c195
Alessander <alessander.logistica@gmail.com>
2020-05-21 23:03:38 -0300

    check if field is not blank

--- src/main/java/com/example/app/model/Tutorial.java
+++ src/main/java/com/example/app/model/Tutorial.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tutorials")
@@ -14,9 +15,11 @@ public class Tutorial {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
+	@NotBlank
 	@Column(name = "title")
 	private String title;
-	
+
+	@NotBlank
 	@Column(name = "description")
 	private String description;
 	
